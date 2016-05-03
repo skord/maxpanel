866e226501cdec7aeb82987fc6ffa5d156702ad3
Mike Danko <mike@l4m3.com>
2016-05-03 14:57:59 -0400

    removing .json addition to urls

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,5 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: 'http://localhost:3000',
-  buildURL: function(record, suffix) {
-    var s = this._super(record,suffix);
-    return s + ".json";
-  }
+  host: 'http://10.190.0.5:8003'
 });
