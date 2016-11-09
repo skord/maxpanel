07573100b55bec1c9f04b71f1ac21ebc4c86086c
Mike Danko <mike@l4m3.com>
2016-11-09 15:22:28 -0500

    errors caught at top router

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 import LoadingSliderMixin from '../mixins/loading-slider';
 
-export default Ember.Route.extend(LoadingSliderMixin, { });
+export default Ember.Route.extend(LoadingSliderMixin,{
+actions: {
+
+  error(error, transition) {
+    this.transitionTo('error');
+  }
+}}
+);
